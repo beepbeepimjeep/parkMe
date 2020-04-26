089f325bf72587e53ad8950a2ef236961312efe3
taeSkylar <42730433+taeSkylar@users.noreply.github.com>
2020-04-27 07:12:02 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        "id":"10001",
+        "first_name":"",
+        "last_name":"",
+        "phone_number":"",
+        "email_address":"",
+        //offer coupon to users/subscribers in their month of birthday
+        "birthday":"",
+        //ask user to leave some comments(optional)
+        "review":"",
+    },
+    {
+        "id":"10002",
+        "first_name":"",
+        "last_name":"",
+        "phone_number":"",
+        "email_address":"",
+        //offer coupon to users/subscribers in their month of birthday
+        "birthday":""
+        //ask user to leave some comments(optional)
+        "review":""
+    },
+    {
+        "id":"10003",
+        "first_name":"",
+        "last_name":"",
+        "phone_number":"",
+        "email_address":"",
+        //offer coupon to users/subscribers in their month of birthday
+        "birthday":"",
+        //ask user to leave some comments(optional)
+        "review":"",
+    }
+];
