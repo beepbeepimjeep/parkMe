3450a13cc65fcd10797544ef4dbc4bac25e19147
Yuhong-Guo <62046117+Yuhong-Guo@users.noreply.github.com>
2020-05-01 11:33:06 +1000

    Create comment.js

---
+++ parkMe-test/models/comment.js
@@ -0,0 +1,14 @@
+const comment = [
+    {
+        userid: "0001",
+        parkid: "0001",
+        comment: "i like it"
+    },
+    {
+        userid: "0002",
+        parkid: "0002",
+        comment: "i dont like it"
+    }
+];
+
+module.exports = comment;
